0ccf7f8c70d7ceb839f4e22eec096ba29daebfd5
Jordan Reiter <jordan@linux.aace.org>
2011-05-18 14:03:23 -0500

    Setup

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='django-email-auth',
+      version='1.0',
+      description='E-mail Authentication',
+      author='Jordan Reiter',
+      author_email='jordanreiter@gmail.com',
+      packages=['email_auth'],
+     )
+
